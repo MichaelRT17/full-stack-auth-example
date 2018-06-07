a2c03e1b240a8a4ba579f9721cfe33e8c46abd50
Michael Thurman <michaelrt17@icloud.com>
2018-06-07 11:33:02 -0600

    server/db in progress

---
+++ instructor-code/server/index.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const express = require('express')
+    , session = require('express-session')
+    , passport = require('passport')
+    , Auth0Strategy = require('passport-auth0')
+    , massive = require('massive');
+
+const {
+    SERVER_PORT,
+    SESSION_SECRET,
+    DOMAIN,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    CALLBACK_URL,
+    CONNECTION_STRING
+} = process.env;
+
+const app = express();
+
+massive(CONNECTION_STRING).then( db => {
+    app.set('db', db);
+})
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new Auth0Strategy({
+    domain: DOMAIN,
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: CALLBACK_URL,
+    scope: 'openid profile'
+}, (accessToken, refreshToken, extraParams, profile, done) => {
+    const db = app.get('db');
+    
+}))
+
+passport.serializeUser((profile, done) => {
+    done(null, profile);
+})
+passport.deserializeUser((profile, done) => {
+    done(null, profile);
+})
+
+app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000'
+}))
+
+
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Listening on port: `, SERVER_PORT)
+})
